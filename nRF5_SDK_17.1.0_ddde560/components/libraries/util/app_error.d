18beba11f1127f2a20ebb94decff4db0be34a9a8
Evan Perry Grove <evan@4grove.com>
2022-08-25 13:07:03 -0500

    Add SDK files

---
+++ nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.d
@@ -0,0 +1,102 @@
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.o: \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.c \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/core_cm4.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\stdint.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\_default_types.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\features.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\_newlib_version.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_intsup.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_stdint.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_version.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_compiler.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_gcc.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/mpu_armv7.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52840.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840_bitfields.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/compiler_abstraction.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\stdio.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\_ansi.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\newlib.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\config.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\ieeefp.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\cdefs.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stdarg.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\reent.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\_ansi.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_types.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\_types.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\lock.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\types.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\endian.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\_endian.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\select.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_sigset.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_timeval.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\timespec.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_timespec.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\sched.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\types.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\stdio.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.h \
+ c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stdbool.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/sdk_errors.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/softdevice/s140/headers/nrf_error.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nordic_common.h \
+ src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error_weak.h
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/core_cm4.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\stdint.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\_default_types.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\features.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\_newlib_version.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_intsup.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_stdint.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_version.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_compiler.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_gcc.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/mpu_armv7.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52840.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840_bitfields.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/compiler_abstraction.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\stdio.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\_ansi.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\newlib.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\config.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\ieeefp.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\cdefs.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stdarg.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\reent.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\_ansi.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_types.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\_types.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\lock.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\types.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\endian.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\_endian.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\select.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_sigset.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_timeval.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\timespec.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_timespec.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\_pthreadtypes.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\sched.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\machine\types.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\arm-none-eabi\include\sys\stdio.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.h:
+c\:\programdata\chocolatey\lib\gcc-arm-embedded\tools\gcc-arm-none-eabi-10-2020-q4-major\lib\gcc\arm-none-eabi\10.2.1\include\stdbool.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/sdk_errors.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/softdevice/s140/headers/nrf_error.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nordic_common.h:
+src/external/nRF5/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error_weak.h:
