bb465281598f7e337ffd07b9c77838e7801fe26c
Evan Perry Grove <evan@4grove.com>
2022-09-03 12:49:29 -0500

    Add UUID and ATTR memory for DFU service

--- config/sdk_config.h
+++ config/sdk_config.h
@@ -11519,12 +11519,12 @@
 
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4. 
 #ifndef NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE
-#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 3136
+#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 3184
 #endif
 
 // <o> NRF_SDH_BLE_VS_UUID_COUNT - The number of vendor-specific UUIDs. 
 #ifndef NRF_SDH_BLE_VS_UUID_COUNT
-#define NRF_SDH_BLE_VS_UUID_COUNT 1
+#define NRF_SDH_BLE_VS_UUID_COUNT 2
 #endif
 
 // <q> NRF_SDH_BLE_SERVICE_CHANGED  - Include the Service Changed characteristic in the Attribute Table.
